1303445a48ae31f72bb4d88df33141ed1a1191df
Andrea Freddi <56250515+Andrea180580@users.noreply.github.com>
2024-10-07 16:21:49 +0200

    Adds final version

--- script/D1.js
+++ script/D1.js
@@ -11,38 +11,51 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let JS =
+  'JavaScript prevede 5 diversi tipi di datatype da associare alle varibili o alle costanti: numerici (quindi numeri come 1, 2 , 3, 4), stringhe (quindi parole e/o nomi come Pippo e Pluto), boleani (quindi condizioni che possono essere VERE o FALSE), undefined (nel caso in cui non si è ancora deciso che valore assegnare alla variabile o costante) o null (nel caso in cui si sia volutamente deciso di lasciare la variabile VUOTA)'
+console.log(JS)
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = 'Andrea'
+console.log('Il mio nome è', myName)
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let number1 = 12
+let number2 = 20
+let sum = number1 + number2
+console.log('Il risultato della somma di number1 e number2 è', sum)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
+console.log('Il valore di x è', x)
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName = 'Freddi'
+console.log('Il mio cognome è', myName)
+const NAME = 'Andrea'
+console.log('Il mio nome è', NAME)
+// NAME = 'Pippo' // in console viene segnato errore alla riga 48 perchè ho provato ad assegnare nuovo valore ad una costante//
+// quindi provando a dire che "il mio nome è Pippo" in console mi verrà restituito un valore boleano "FALSE" //
+console.log('Mi chiamo Pippo è', NAME === 'Pippo')
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log('Il risultato della sottrazione (4 - x) è pari a', 4 - x)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +63,14 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = 'john'
+let name2 = 'John'
+console.log('La condizione "name1 e name2 sono uguali" è', name1 === name2)
+
+// per fare diventare true serve che entrambe le stringhe siano lowercase (quindi senza più la J in maiuscolo) //
+// per poterlo fare si applica toLowerCase() //
+
+console.log(
+  'Applicando "toLowerCase()" la condizione "name1 e name2 sono uguali" diventa',
+  name1 === name2.toLowerCase()
+)
